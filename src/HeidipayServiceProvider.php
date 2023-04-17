b2cf55215ae3c4b90676d7eaa2ac1a0f68a526c0
Simone Folador <sfolador@gmail.com>
2023-04-17 22:02:28 +0200

    amended heidipay construct in provider

--- src/HeidipayServiceProvider.php
+++ src/HeidipayServiceProvider.php
@@ -23,8 +23,11 @@ class HeidipayServiceProvider extends PackageServiceProvider
     public function packageRegistered(): void
     {
         $this->app->bind(HeidipayInterface::class, function () {
+
             return new Heidipay(
+            /** @phpstan-ignore-next-line  */
                 apiUrl: config('heidipay.api_url'),
+                /** @phpstan-ignore-next-line  */
                 merchantKey: config('heidipay.merchant_key')
             );
         });
